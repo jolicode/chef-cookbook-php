6498db37fa96a4d04f4952530ec9a003cfa7e498
Joel Wurtz <jwurtz@jolicode.com>
2013-05-06 16:57:40 +0200

    Set composer create project action idempotent fix #2

--- providers/composer.rb
+++ providers/composer.rb
@@ -26,13 +26,17 @@ end
 
 action :create_project do
   if !new_resource.package
-    raise ArgumentError, "You must provide a package option to composer resource with create_project action"
+    raise ArgumentError, "You must provide a package option to composer resource with create_project action."
   end
 
-  execute "composer create-project" do
-    command "composer create-project #{new_resource.package} #{new_resource.directory} #{new_resource.version} #{new_resource.options}"
-    cwd new_resource.cwd
-    environment new_resource.env_vars
+  if !Dir.exists?(new_resource.directory) || (Dir.entries(new_resource.directory) - %w{ . .. }).empty?
+    execute "composer create-project" do
+      command "composer create-project #{new_resource.package} #{new_resource.directory} #{new_resource.version} #{new_resource.options}"
+      cwd new_resource.cwd
+      environment new_resource.env_vars
+    end
+  else
+    Chef::Log.warn "Directory #{new_resource.directory} exists and not empty, project #{new_resource.package} will not be installed."
   end
 
   new_resource.updated_by_last_action(true)
